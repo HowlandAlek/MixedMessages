0bb3c49a7d55038fb437ac581b4eb0ea643137c3
ahowland <howlandalek@gmail.com>
2021-05-05 20:28:25 -0500

    feat: getters and quote generator.

--- main.js
+++ main.js
@@ -18,10 +18,74 @@ let data = {
 		],
 	],
 	_farewells: [["Good-bye."], ["See you soon."], ["See you then."]],
-	_types: ["Motivational ", "Entrepreneurs", "Hustle"],
-	get greeting() {},
-	get quote() {},
-	get farewell() {},
-	get type() {},
-	createQuote() {},
+	_types: ["Motivational", "Entrepreneurs", "Hustle"],
+	get greeting() {
+		let date = new Date();
+		let hour = date.getHours();
+
+		let rndIndex = 0;
+
+		if (hour < 12) {
+			rndIndex = Math.floor(Math.random() * this._greetings[0].length);
+			return this._greetings[0][rndIndex];
+		} else if (hour > 12 && hour < 19) {
+			rndIndex = Math.floor(Math.random() * this._greetings[1].length);
+			return this._greetings[1][rndIndex];
+		} else {
+			rndIndex = Math.floor(Math.random() * this._greetings[2].length);
+			return this._greetings[2][rndIndex];
+		}
+	},
+	get type() {
+		let rndIndex = Math.floor(Math.random() * this._types.length);
+		return this._types[rndIndex];
+	},
+	get quote() {
+		let type = this.type;
+		let rndIndex = 0;
+
+		switch (type) {
+			case "Motivational":
+				rndIndex = Math.floor(Math.random() * this._greetings[0].length);
+				return this._quotes[0][rndIndex];
+				break;
+			case "Entrepreneurs":
+				rndIndex = Math.floor(Math.random() * this._greetings[1].length);
+				return this._quotes[1][rndIndex];
+				break;
+			case "Hustle":
+				rndIndex = Math.floor(Math.random() * this._greetings[2].length);
+				return this._quotes[2][rndIndex];
+				break;
+			default:
+				return "Oops we ran out of quotes";
+				break;
+		}
+	},
+	get farewell() {
+		let date = new Date();
+		let hour = date.getHours();
+
+		let rndIndex = 0;
+
+		if (hour < 12) {
+			rndIndex = Math.floor(Math.random() * this._farewells[0].length);
+			return this._farewells[0][rndIndex];
+		} else if (hour > 12 && hour < 19) {
+			rndIndex = Math.floor(Math.random() * this._farewells[1].length);
+			return this._farewells[1][rndIndex];
+		} else {
+			rndIndex = Math.floor(Math.random() * this._farewells[2].length);
+			return this._farewells[2][rndIndex];
+		}
+	},
+	createQuote() {
+		let greeting = this.greeting;
+		let quote = this.quote;
+		let farewell = this.farewell;
+
+		return `${greeting}, your quote:\n${quote}\n${farewell}`;
+	},
 };
+
+console.log(data.createQuote());
